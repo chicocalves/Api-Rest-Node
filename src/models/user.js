9c492d8e913b746f34c2a293554d583c6318a4e9
Francisco Alves <fcoocf@hotmail.com>
2022-06-07 01:02:27 +0000

    Criando users com password criptografado

---
+++ src/models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const bcript = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    createAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+UserSchema.pre('save', async function(next) {
+    const hash = await bcript.hash(this.password, 10);
+    this.password = hash;
+
+    next();
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
