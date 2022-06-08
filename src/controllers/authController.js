e48a20a7509fd5cc1b703b50964d0d98167454d8
Francisco Alves <fcoocf@hotmail.com>
2022-06-08 00:37:19 +0000

    Validação de token e acessos

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,8 +2,17 @@ const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
 const connectToDatabase = require('../database');
+const bcript = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth.json') 
 
 connectToDatabase();
+
+function generateToken (params = {}){
+    return jwt.sign(params, authConfig.secret, {
+        expiresIn: 86400,
+    });
+};
 
 router.post('/register', async (req, res) => {
     try {
@@ -14,10 +23,23 @@ router.post('/register', async (req, res) => {
 
         const user = await User.create(req.body);
         user.password = undefined;
-        res.send({ user });
+        res.send({ user, token: generateToken({id: user.id})});
     } catch (error) {
         return res.status(400).send({ error: 'Registration failed'});
     }
 });
+
+router.post('/authenticate', async (req, res) => {
+    const {email, password} = req.body;
+    const user = await User.findOne({email}).select('+password');
+
+    if(!user || !await bcript.compare(password, user.password)){
+        return res.status(400).send({ error: 'User ou password not found'});
+    };
+
+    user.password = undefined;
+
+    res.send({ user, token: generateToken({id: user.id})});
+})
 
 module.exports = app => app.use('/auth', router);
---
+++ src/controllers/projectController.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const authMiddleware = require('../middlewares/auth')
+
+const router = express.Router();
+
+router.use(authMiddleware);
+
+router.get('/', (req, res) => {
+
+    console.log('project ' + req.userId);
+
+    res.send({ ok: true, user: req.userId });
+});
+
+module.exports = app => app.use('/projetcs', router);
+
